4f0fcf75166e70ada774a8f35ed1c0c6835cdd8f
asaad-tt <aasadshumon86@gmail.comm>
2022-10-25 10:56:26 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBkZXePDKvhuGyEZKvv0d5MdTfBfZ8Qeag",
+  authDomain: "learnex-academy.firebaseapp.com",
+  projectId: "learnex-academy",
+  storageBucket: "learnex-academy.appspot.com",
+  messagingSenderId: "672647774364",
+  appId: "1:672647774364:web:b8450206eef3024440ad59",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
